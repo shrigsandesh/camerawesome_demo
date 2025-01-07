0f204c689323bf8de876d54694a6ffdff5025773
shrigsandesh <sandesh@shrigsolutions.com>
2025-01-07 14:23:16 +0545

    aspect ratio widget added

--- lib/custom_camera/widgets/camera_actions/top_action_bar.dart
+++ lib/custom_camera/widgets/camera_actions/top_action_bar.dart
@@ -1,4 +1,5 @@
 import 'package:camerawesome/camerawesome_plugin.dart';
+import 'package:camerawesome_demo/extensions/context_extensions.dart';
 import 'package:flutter/material.dart';
 
 class TopActionBar extends StatelessWidget {
@@ -10,14 +11,89 @@ class TopActionBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          if (state != null)
-            FlashButton(
-              state: state!,
-            ),
-        ],
-      ),
+      child: state != null
+          ? Row(
+              children: [
+                FlashButton(
+                  state: state!,
+                ),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                InkWell(
+                  onTap: () {
+                    var currentRatio = state?.sensorConfig.aspectRatio;
+                    switch (currentRatio) {
+                      case CameraAspectRatios.ratio_16_9:
+                        currentRatio = CameraAspectRatios.ratio_4_3;
+                        break;
+                      case CameraAspectRatios.ratio_4_3:
+                        // Check if running on iOS before switching to 1:1
+                        if (Theme.of(context).platform == TargetPlatform.iOS) {
+                          currentRatio = CameraAspectRatios.ratio_1_1;
+                        } else {
+                          currentRatio = CameraAspectRatios.ratio_16_9;
+                        }
+                        break;
+                      case CameraAspectRatios.ratio_1_1:
+                        currentRatio = CameraAspectRatios.ratio_16_9;
+                        break;
+                      case null:
+                        currentRatio = CameraAspectRatios.ratio_4_3;
+                    }
+                    state?.sensorConfig.setAspectRatio(currentRatio);
+                  },
+                  child: AspectRatioButton(
+                    state: state!,
+                  ),
+                ),
+              ],
+            )
+          : null,
+    );
+  }
+}
+
+class AspectRatioButton extends StatelessWidget {
+  final CameraState state;
+
+  const AspectRatioButton({
+    super.key,
+    required this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<SensorConfig>(
+      key: const ValueKey("ratioButton"),
+      stream: state.sensorConfig$,
+      builder: (_, sensorConfigSnapshot) {
+        if (!sensorConfigSnapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+        final sensorConfig = sensorConfigSnapshot.requireData;
+        return StreamBuilder<CameraAspectRatios>(
+          stream: sensorConfig.aspectRatio$,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox.shrink();
+            }
+
+            return AwesomeOrientedWidget(
+                child: switch (snapshot.data) {
+              CameraAspectRatios.ratio_16_9 => Text(
+                  '16:9',
+                  style: context.bodyMedium,
+                ),
+              CameraAspectRatios.ratio_4_3 =>
+                Text('4:3', style: context.bodyMedium),
+              CameraAspectRatios.ratio_1_1 =>
+                Text('1:1', style: context.bodyMedium),
+              null => Text('4:3', style: context.bodyMedium),
+            });
+          },
+        );
+      },
     );
   }
 }
